9882d640be6ad1380b47faf40b93cb3c044def84
Abolfazl Mohammadnezhad <109286761+ABMABOLFAZLBUSI@users.noreply.github.com>
2026-06-30 22:43:02 +0000

    added icons and fixed data save file problem

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.abolfazldigital.focussanctum',
   appName: 'FocusSanctum',
-  webDir: 'dist'
+  webDir: 'dist',
+  assets: {
+    icon: {
+      android: 'assets/icon.png',   // Path to your source image
+    },
+    splash: {
+      android: 'assets/splash.png', // (Optional) Add a splash screen image
+    },
+  },
 };
 
 export default config;
